f732a785880a0f94606a04a884d2ab8ebf1fa041
Steve Stranczek <stevestranczek@gmail.com>
2019-03-19 14:55:50 -0500

    Added a camera switch function

---
+++ CS426AntarcticEnigmaSWPrototype/Assets/CameraSwitch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+	public GameObject PlayerCamera;
+	public GameObject ComputerCamera;
+
+	public int CamMode;
+	// Start is called before the first frame update
+	void Start()
+	{
+		CamMode = 0;
+		PlayerCamera.SetActive(true);
+		ComputerCamera.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void GoToPlayerCamera()
+	{
+		PlayerCamera.SetActive(true);
+		ComputerCamera.SetActive(false);
+	}
+
+	//IEnumerator PlayerCamSwitch()
+	//{
+	//	yield return new WaitForSeconds(0.01f);
+	//	PlayerCamera.SetActive(true);
+	//	ComputerCamera.SetActive(false);
+	//}
+
+	public void GoToComputerCamera()
+	{
+		PlayerCamera.SetActive(false);
+		ComputerCamera.SetActive(true);
+	}
+
+	//IEnumerator ComputerCamSwitch()
+	//{
+	//	yield return new WaitForSeconds(0.01f);
+	//	PlayerCamera.SetActive(true);
+	//	ComputerCamera.SetActive(false);
+	//}
+}
--- CS426AntarcticEnigmaSWPrototype/Assets/SimpleMovement.cs
+++ CS426AntarcticEnigmaSWPrototype/Assets/SimpleMovement.cs
@@ -9,34 +9,72 @@ public class SimpleMovement : MonoBehaviour
 	public float rotationSpeed = 90;
 	public float force = 700f;
 
+	private CameraSwitch camswitch;
 
 	Rigidbody rb;
 	Transform t;
 
+	public int GameMode; //0 if player controled 1 for computer 2 for kitchen....etc.....
+	public bool CanGoToComputer;
 
 	// Use this for initialization
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
 		t = GetComponent<Transform>();
+		GameMode = 0;
+		CanGoToComputer = false;
+		camswitch = GameObject.FindGameObjectWithTag("GameController").GetComponent<CameraSwitch>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetKey(KeyCode.W))
-			rb.velocity += this.transform.forward * speed * Time.deltaTime;
-		else if (Input.GetKey(KeyCode.S))
-			rb.velocity -= this.transform.forward * speed * Time.deltaTime;
+		if (GameMode == 0)
+		{ 
+			if (Input.GetKey(KeyCode.W))
+				rb.velocity += this.transform.forward * speed * Time.deltaTime;
+			else if (Input.GetKey(KeyCode.S))
+				rb.velocity -= this.transform.forward * speed * Time.deltaTime;
 
-		if (Input.GetKey(KeyCode.D))
-			rb.rotation *= Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0);
-		else if (Input.GetKey(KeyCode.A))
-			rb.rotation *= Quaternion.Euler(0, -rotationSpeed * Time.deltaTime, 0);
+			if (Input.GetKey(KeyCode.D))
+				rb.rotation *= Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0);
+			else if (Input.GetKey(KeyCode.A))
+				rb.rotation *= Quaternion.Euler(0, -rotationSpeed * Time.deltaTime, 0);
 
-		if (Input.GetKeyDown(KeyCode.Space))
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				if (CanGoToComputer)
+				{
+					Debug.Log("Trying to switch camera.");
+					camswitch.GoToComputerCamera();
+					GameMode = 1;
+				}
+			}
+		}
+		else if (GameMode == 1)
 		{
-		//	rb.AddForce(t.up * force);
+			if(Input.GetKey(KeyCode.Escape))
+			{
+				camswitch.GoToPlayerCamera();
+				GameMode = 0;
+			}
+		}
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Computer")
+		{
+			CanGoToComputer = true;
+		}
+	}
+
+	public void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Computer")
+		{
+			CanGoToComputer = false;
 		}
 	}
 }
