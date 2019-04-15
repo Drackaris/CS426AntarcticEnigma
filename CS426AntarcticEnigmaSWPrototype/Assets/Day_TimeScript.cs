282da5a59e26a0631602b62b653d4adf128b14a0
Steve Stranczek <stevestranczek@gmail.com>
2019-04-15 13:15:17 -0500

    Added a clock in game, time will have to be adjusted

---
+++ CS426AntarcticEnigmaSWPrototype/Assets/Day_TimeScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day_TimeScript : MonoBehaviour
+{
+	public int day;
+	public float hour;
+	public float minutes;
+
+	public float lastChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+		day = 0;
+		hour = 6;
+		minutes = 0;
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Time.time - lastChange > 30.0)
+		{
+			minutes++;
+			if (minutes == 6)
+			{
+				minutes = 0;
+				hour++;
+				if (hour == 24)
+				{
+
+
+				}
+			}
+			lastChange = Time.time;
+			Debug.Log(hour.ToString() + ":" + minutes.ToString());
+		}
+	}
+}
