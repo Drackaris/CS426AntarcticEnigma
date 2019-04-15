933a2026a0e76859a42d268f81b617117fc6bc1f
Steve Stranczek <stevestranczek@gmail.com>
2019-04-15 18:58:21 -0500

    Added a new scene with a very basic rough prototype of a working ai

---
+++ CS426AntarcticEnigmaSWPrototype/Assets/NpcMovementScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcMovementScript : MonoBehaviour
+{
+	public Day_TimeScript DTScript;
+	public Transform Computer;
+	public Transform Kitchen;
+	NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		agent = GetComponent<NavMeshAgent>();
+		DTScript = GameObject.Find("TimeController").GetComponent<Day_TimeScript>();
+    }
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "SlideDoor")
+		{
+			Animator animator = other.GetComponent<Animator>();
+			animator.SetTrigger("DoorOpen");
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (DTScript.minutes < 1)
+		{
+			agent.SetDestination(Computer.position);
+		}
+		else if (DTScript.minutes >= 2)
+		{
+			agent.SetDestination(Kitchen.position);
+		}
+    }
+}
