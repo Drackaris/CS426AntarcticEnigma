149066fa2912f347770a29569b960d481021430f
Steve Stranczek <stevestranczek@gmail.com>
2019-04-16 10:49:45 -0500

    Made some more changes with the sleeping, still not perfect

--- CS426AntarcticEnigmaSWPrototype/Assets/NpcMovementScript.cs
+++ CS426AntarcticEnigmaSWPrototype/Assets/NpcMovementScript.cs
@@ -8,6 +8,9 @@ public class NpcMovementScript : MonoBehaviour
 	public Day_TimeScript DTScript;
 	public Transform Computer;
 	public Transform Kitchen;
+	public Transform Bed;
+	private bool Sleep;
+	private bool Sleeping;
 	NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -24,16 +27,41 @@ public class NpcMovementScript : MonoBehaviour
 			Animator animator = other.GetComponent<Animator>();
 			animator.SetTrigger("DoorOpen");
 		}
+		if(other.tag == "Bed")
+		{
+			if(Sleep == true)
+			{
+				this.GetComponent<NavMeshAgent>().enabled = false;
+				this.GetComponent<Rigidbody>().isKinematic = true;
+				this.GetComponent<Rigidbody>().useGravity = false;
+				this.transform.position = new Vector3(15.405f, 2.027f, -26.66f);
+				this.transform.Rotate(90, 270, 0, Space.World);
+				Sleeping = true;
+			}
+		}
 	}
 
     // Update is called once per frame
     void Update()
     {
-		if (DTScript.minutes < 1)
+		if (DTScript.minutes == 0)
 		{
+			Sleep = true;
+			if (Sleeping == false)
+			{ 
+				agent.SetDestination(Bed.position);
+			}
+		}
+		if (DTScript.minutes >= 1)
+		{
+			Sleep = false;
+			Sleeping = false;
+			this.GetComponent<NavMeshAgent>().enabled = true;
+			this.GetComponent<Rigidbody>().isKinematic = false;
+			this.GetComponent<Rigidbody>().useGravity = true;
 			agent.SetDestination(new Vector3(Computer.position.x,Computer.position.y,Computer.position.z - 2f));
 		}
-		else if (DTScript.minutes >= 2)
+		else if (DTScript.minutes >= 3)
 		{
 			agent.SetDestination(new Vector3(Kitchen.position.x, Kitchen.position.y, Kitchen.position.z - 2f));
 		}
