477ad38bea0b4a0b38ecebca95e605aaf5850d2c
Mohmed Hira <mhira2@uic.edu>
2019-04-29 20:39:50 -0500

    KitchenGame Audio Glitch
    Had to fix a kitchen game audio glitch

--- CS426AntarcticEnigmaSWPrototype/Assets/panSelector.cs
+++ CS426AntarcticEnigmaSWPrototype/Assets/panSelector.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections.Generic; using UnityEngine; using Random = System.Random;  public class panSelector : MonoBehaviour {     public Material highlightMat;     public Material normalMat;     public int highlighted;     public GameObject pan1, pan2, pan3;     public ParticleSystem smoke, lightt, burning, lighttt, lightttt, burningg, burninggg;     public GameObject[] MyObjects;     public ParticleSystem[] parT2;     public ParticleSystem[] parT3;     public ParticleSystem[] particalSys;     Dictionary<GameObject, ParticleSystem[]> dict = new Dictionary<GameObject, ParticleSystem[]>();      public SmokeAudio sA;     public OrangeAudio oA;     public OrangeHelperAudio ohA;     public LightFireAudio lfA;      public WinSound wS;     public LoseSound lS;      public int onGreen;      bool secondOn = false;      public SimpleMovement s;      public int counter;     int Stat;     public int totalT;       // Start is called before the first frame update     void Start()     {         MyObjects = new GameObject[4];         MyObjects[1] = pan1;         MyObjects[2] = pan2;         MyObjects[3] = pan3;          GameObject s = MyObjects[2];         s.GetComponentInChildren<Renderer>().material = highlightMat;         highlighted = 2;           particalSys = new ParticleSystem[4];         //particalSys[1] = smoke;         particalSys[2] = lightt;         particalSys[3] = burning;         dict.Add(pan1, particalSys);          parT2 = new ParticleSystem[4];         parT2[2] = lighttt;         parT2[3] = burningg;         dict.Add(pan2, parT2);          parT3 = new ParticleSystem[4];         parT3[2] = lightttt;         parT3[3] = burninggg;         dict.Add(pan3, parT3);          onGreen = 0;          ParticleSystem[] part = dict[MyObjects[2]];         part[2].Play();
+﻿using System.Collections.Generic; using UnityEngine; using Random = System.Random;  public class panSelector : MonoBehaviour {     public Material highlightMat;     public Material normalMat;     public int highlighted;     public GameObject pan1, pan2, pan3;     public ParticleSystem smoke, lightt, burning, lighttt, lightttt, burningg, burninggg;     public GameObject[] MyObjects;     public ParticleSystem[] parT2;     public ParticleSystem[] parT3;     public ParticleSystem[] particalSys;     Dictionary<GameObject, ParticleSystem[]> dict = new Dictionary<GameObject, ParticleSystem[]>();      public SmokeAudio sA;     public OrangeAudio oA;     public OrangeHelperAudio ohA;     public LightFireAudio lfA;      public WinSound wS;     public LoseSound lS;      public int onGreen;      bool secondOn = false;      public SimpleMovement s;      public int counter;     int Stat;     public int totalT;      public int oldRand = 2;       // Start is called before the first frame update     void Start()     {         MyObjects = new GameObject[4];         MyObjects[1] = pan1;         MyObjects[2] = pan2;         MyObjects[3] = pan3;          GameObject s = MyObjects[2];         s.GetComponentInChildren<Renderer>().material = highlightMat;         highlighted = 2;           particalSys = new ParticleSystem[4];         //particalSys[1] = smoke;         particalSys[2] = lightt;         particalSys[3] = burning;         dict.Add(pan1, particalSys);          parT2 = new ParticleSystem[4];         parT2[2] = lighttt;         parT2[3] = burningg;         dict.Add(pan2, parT2);          parT3 = new ParticleSystem[4];         parT3[2] = lightttt;         parT3[3] = burninggg;         dict.Add(pan3, parT3);          onGreen = 0;          ParticleSystem[] part = dict[MyObjects[2]];         part[2].Play();
 
         counter = 0;         Stat = 1;         totalT = 0;        }      // Update is called once per frame     void Update()     {         if(s.inK == 1 && Stat == 1)
         {
-                     sA.audioSource.loop = true;             lfA.audioSource.loop = true;
+                     //sA.audioSource.loop = true;             lfA.audioSource.loop = true;
 
             lfA.audioSource.Play();
-            sA.audioSource.Play();
-            oA.audioSource.Play();             Stat = 0; 
+            //sA.audioSource.Play();
+            //oA.audioSource.Play();             Stat = 0; 
         }          if(s.inK == 0)
         {
             sA.audioSource.loop = false;             lfA.audioSource.loop = false;             sA.audioSource.Stop();             oA.audioSource.Stop();             lfA.audioSource.Stop();             Stat = 1;         }           if (s.counter == 3)
         {
             //wS.audioSource.Play(); 
-            s.kDone = true;             //s.counter = 0;             Stat = 1;             totalT = 0;
+            s.kDone = true;             lfA.audioSource.Stop();             lfA.audioSource.loop = false;             //lfA.audioSource.loop = true;             //lfA.audioSource.Play();             //s.counter = 0;             Stat = 1;             totalT = 0;
         }          if(s.chances == 10)
         {
             sA.audioSource.loop = false;             lfA.audioSource.loop = false;             sA.audioSource.Stop();             oA.audioSource.Stop();             lfA.audioSource.Stop();
@@ -27,14 +27,18 @@
                 part[3].Stop();                 secondOn = false;                 ohA.audioSource.loop = false;                 ohA.audioSource.Stop();
             }             else
             {
-                part[2].Stop();                  if (s.counter < 4)
+                part[2].Stop();                 lfA.audioSource.loop = false;                 lfA.audioSource.Stop();                  if (s.counter < 4)
                 {
                     System.Random random = new Random();
-                    int randomNumber = random.Next(1, 4);
+                    int randomNumber = random.Next(1, 4);                      while(randomNumber == oldRand)
+                    {                         random = new Random();
+                        randomNumber = random.Next(1, 4);
+                    }                     oldRand = randomNumber;
+
 
                     ParticleSystem[] partT = dict[MyObjects[randomNumber]];
 
-                    partT[2].Play();
+                    partT[2].Play();                     lfA.audioSource.loop = true;                     lfA.audioSource.Play();
                 }                  s.counter += 1;
             }
                      s.onGreen = 0;
@@ -43,7 +47,4 @@
 
         }
 
-
-        if (Input.GetKeyDown(KeyCode.O) )         {             for (int i = 1; i < 4; i++)             {                 ParticleSystem[] part = dict[MyObjects[i]];                  //part[1].Stop();                 part[2].Stop();                 part[3].Stop();                  sA.audioSource.loop = false;                 lfA.audioSource.loop = false;                 ohA.audioSource.loop = false;                  sA.audioSource.Stop();                 oA.audioSource.Stop();                 lfA.audioSource.Stop();                 ohA.audioSource.Stop();               }          }         else if(Input.GetKeyDown(KeyCode.P))         {             for (int i = 1; i < 4; i++)             {                 ParticleSystem[] part = dict[MyObjects[i]];                  //part[1].Play();                 part[2].Play();                 part[3].Play();
-
-                sA.audioSource.loop = true;                 lfA.audioSource.loop = true;                 ohA.audioSource.loop = true;                  lfA.audioSource.Play();                 sA.audioSource.Play();                 oA.audioSource.Play();                  ohA.audioSource.Play();                 }         }      } } 
+      } } 
